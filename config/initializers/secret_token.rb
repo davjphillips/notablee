b2f7a035708a0ca642ac4cd8b14ce2ac8747c651
David J. Phillips <D@sutterstudios.com>
2012-03-13 16:16:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notablee::Application.config.secret_token = '3e8c29298a35509a5986214b23badfb0252df01c7baf1906d99f4ce9ceecf923c92524b04e827a2f32c4fb438e50d69d368b85619593309f980333b68497c68f'
