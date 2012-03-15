b494c501b50b26910dd99d251e18d774caec5f2d
Mehul Kar <mehul.kar@gmail.com>
2012-03-15 12:46:14 -0700

    fixes bug for duplicate authentications

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationsController < ApplicationController
   
   def create
     auth = request.env["omniauth.auth"]
-    current_user.authentications.create(:provider => auth['provider'], :uid => auth['uid'])
+    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
     flash[:notice] = "Authentication Successful"
     redirect_to root_path
   end
