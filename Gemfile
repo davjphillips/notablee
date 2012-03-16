c016b3219e111a76052c70b889927e240fa48afa
Mehul Kar <mehul.kar@gmail.com>
2012-03-16 15:44:31 -0700

    adds twitter gem

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.1'
 gem 'devise', '~>2.0.4'
 gem 'omniauth', '~>1.0.3'
 gem 'omniauth-twitter'
+gem 'twitter', '~>2.1.1'
 
 
 # Bundle edge Rails instead:
--- spec/requests/authentications_spec.rb
+++ spec/requests/authentications_spec.rb
@@ -6,6 +6,7 @@ describe "Authentications" do
     it "twitter sign in button should lead to twitter authentication page" do
       visit root_path
       click_link "Sign in with Twitter"
+      page.
       Authentication.last.uid.should == '123545'
     end
   end
