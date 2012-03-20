721f043fd1edbbc1a885bd08b37feb428c566638
Mehul Kar <mehul.kar@gmail.com>
2012-03-20 16:55:25 -0700

    refactors authentication specs

--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -1,36 +1,28 @@
 require 'spec_helper'
 
 describe AuthenticationsController do
-
+  before(:each) do
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+    @user = User.new(:email => 'b@ba.com', :password => 'blahblah')
+  end
+  
   describe '#create' do
     context 'when authentication is found' do
-      before :each do
-        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
-        @user = User.create!(:email => 'b@b.com', :password => 'blahblah')
-        @auth = Authentication.create!(:provider => 'twitter', :uid => '123545', :user => @user)
-      end
       it 'flashes a success message' do
+        @user.save!
+        @auth = Authentication.create!(:provider => 'twitter', :uid => '123545', :user => @user)
         post :create
         flash[:notice].should == "Authentication Successful with Twitter"
       end  
     end
     
-    context 'when authentication is not found' do
-      context 'when current_user exists' do
-        
-      end
-      
+    context 'when authentication is not found' do      
       context 'when current_user does not exist' do
-        before :each do
-          request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
-          user = User.new(:email => 'b@ba.com', :password => 'blahblah')
-          User.stub(:new_user_with_auth).and_return(user)
-          
-          #@auth = Authentication.create!(:provider => 'twitter', :uid => '123545')
-        end
-        
         it "creates a new user" do
-          expect {post :create}.to change(User, :count).by(1)
+          User.stub(:new_user_with_auth).and_return(@user)
+          expect {
+            post :create
+          }.to change(User, :count).by(1)
         end
       end
     end
